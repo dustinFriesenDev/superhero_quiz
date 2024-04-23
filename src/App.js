18027260b6ee589d3549ebf423b8134ed4dcaf66
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-04-23 11:04:14 -0400

    html in place. working on css and js animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import princesses from './superheroDataSheet.json';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [addPoint, setAddPoint] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const correctAnswer = (e) => {
+    const oc = document.getElementById("option-container");
+    const answer = document.getElementById("answer");
+    const option = e.target;
+    const correct = document.getElementById("correct");
+    if(option.innerText === correct.innerText){
+      oc.style.display = "none";
+      answer.style.display = "block";
+      setAddPoint(addPoint + 1);
+    } else {
+      if(option.id !== "option-container"){
+        option.style.backgroundColor = "#054553";
+        option.style.color = "white";
+        option.style.opacity = "0.7";
+        option.classList.add("add-shake");
+        setAddPoint(addPoint - 1);
+      }
+    }
+    console.log(addPoint);
+  }
+
+  const nextQuestion = () => {
+    setCounter(counter + 1);
+    if(addPoint > 0){
+      setScore(score + 1);
+    }
+    setAddPoint(0);
+    console.log(addPoint);
+    console.log(score);
+  }
+
+  const princessList = princesses.map((e) => {
+    if(counter === e.id){
+      return (
+        <div key={e.id}>
+          <div className="question" id="question">{e.id}. {e.question}</div>
+          <div className='option-container' id="option-container" onClick={correctAnswer}>
+            <p className='option o1' id="o1">{e.o1}</p>
+            <p className='option o2' id="o2">{e.o2}</p>
+            <p className='option o3' id="o3">{e.o3}</p>
+            <p className='option o4' id="o4">{e.o4}</p>
+          </div>
+
+        {/* Answer */}
+          <div className='answer' id="answer">
+            <h3>{e.name}</h3>
+            <p style={{display: "none"}} id="correct">{e.answer}</p>
+            {/* Working on creating a border layer to make the image an oval without having to alter the image. */}
+            {/* <div className='border'></div> */}
+            <img src={e.img} alt={e.altText} />
+            <p className='option btn-next' id="btnNext" onClick={nextQuestion}>Next</p>
+          </div>
+        </div>
+      )
+    } 
+  });
+
+  function endPage() {
+    if (counter > princesses.length){
+      return (
+        <div>
+          <h3>Congratulations!! You finished the quiz.</h3>
+          <p>You got {score} out of {princesses.length} answers correct on the first try.</p>
+          <p>Click the button below to start again.</p>
+          <p className='option btn-next btn-again' id="btnAgain" onClick={startOver}>Start Over</p>
+        </div>
+      )
+    }
+  }
+
+  const startOver = () => {
+    setCounter(1);
+    setAddPoint(0);
+    setScore(0);
+  }
+
+//HTML Return Statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">      
+        <div>Superhero Quiz</div>
       </header>
+        <div className='main-container'>
+          {princessList}
+          {endPage()}
+        </div>
+      <footer>
+        <small>Quiz created by Dustin Friesen. All images and characters are owned by Marvel and DC Comics . This quiz is for demonstration purposes only.</small>
+        <small><a href="https://www.vecteezy.com/free-png/comic-bang">Comic Bang PNGs by Vecteezy</a></small>
+      </footer>
     </div>
   );
 }
